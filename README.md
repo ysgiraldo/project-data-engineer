691a8768d2602fbe6f050ee15d38178f9b837db0
ysgiraldo <ygiraldoca@unal.edu.co>
2024-04-19 16:34:16 -0500

    A project with data engineer techniques

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# project-data-engineer
+# Data engineer
+
+This is a project about data engineering, it contains an orchestration process that goes from the creation of tables to having the project in containers.
---
+++ src/6b_api_server/autograding.py
@@ -0,0 +1,11 @@
+"""Autograding script"""
+
+import os
+
+# test code files
+assert os.path.exists("src/6b_api_server/config.json")
+assert os.path.exists("src/6b_api_server/server.py")
+
+
+# test run
+assert os.path.exists("datalake/logs/api_server.log")
